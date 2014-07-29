b89d5e5c1ad353a8ad97556de50d616b68bfb712
jrhicks@cteh.com <ghfrog78>
2014-07-29 15:45:39 -0500

    change api

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function pipeline() {
 }
 
 function src() {
-    var s1 = core.src_modules()
-        .pipe(core.build_module())
+    var s1 = core.src_themes()
+        .pipe(core.gulp_theme())
         .pipe(core.emit_pages());
     var s2 = core.src_pages();
     return es.merge(s1,s2);
@@ -22,17 +22,17 @@ function src() {
 function watch() {
     var s0 = src();
     var s1 = core.watch_pages();
-    var s2 = core.watch_modules()
-        .pipe(core.build_module())
+    var s2 = core.watch_themes()
+        .pipe(core.gulp_theme())
         .pipe(core.emit_pages());
     return es.merge(s0, s1, s2);
 }
 
 function install() {
-    var s1 = gulp.src('./wikismith_modules/*/bower.json')
+    var s1 = gulp.src('./wikismith_themes/*/bower.json')
         .pipe(core.bower_install());
 
-    var s2 = gulp.src('./wikismith_modules/*/package.json')
+    var s2 = gulp.src('./wikismith_themes/*/package.json')
         .pipe(core.npm_install());
 
     return es.merge(s1, s2);
