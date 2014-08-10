ee38e65db34df7856301902e7525cdabf1bdfca4
jrhicks@cteh.com <ghfrog78>
2014-08-10 18:50:24 -0500

    make theme and pages folders variable

--- README.md
+++ README.md
@@ -14,11 +14,15 @@ Wikismith is a fun static site generator for fans of gulp and bower
 
 * gulp
 
+* Open http://localhost:9292
+
 ## Learn more
 
 * http://wikismith.com
 
 ## Updates
+
+* 8/4/2014 - Added bespoke.js presentations to wikismith_themes
 
 * 8/3/2014 - Added blog functionality via page_snippets.
 
--- index.js
+++ index.js
@@ -3,48 +3,49 @@ var gulp = require('gulp');
 var es = require('event-stream');
 var fs = require('fs');
 
-function pipeline() {
+function pipeline(theme_dir, page_dir) {
     return es.pipeline(
-        core.render_page(),
-        core.unfolder_index(),
-        core.page_snippet(),
-        core.wiki()
+        core.render_page(theme_dir),
+        core.unfolder_index(page_dir),
+        core.index_entry(page_dir),
+        core.wiki(page_dir)
     );
 }
 
-function src() {
-    var s1 = core.src_themes()
-        .pipe(core.gulp_theme())
-        .pipe(core.emit_pages());
-    var s2 = core.src_pages();
+function src(theme_dir, page_dir) {
+    var s1 = core.src_themes(theme_dir)
+        .pipe(core.gulp_theme(theme_dir))
+        .pipe(core.emit_pages(page_dir));
+    var s2 = core.src_pages(page_dir);
     return es.merge(s1,s2);
 }
 
-function watch() {
+function watch(theme_dir, page_dir) {
     var s0 = src();
-    var s1 = core.watch_pages()
-        .pipe(core.emit_dependent());
-    var s2 = core.watch_themes()
-        .pipe(core.gulp_theme())
-        .pipe(core.emit_pages());
+    var s1 = core.watch_pages(page_dir)
+        .pipe(core.emit_dependent(page_dir));
+    var s2 = core.watch_themes(theme_dir)
+        .pipe(core.gulp_theme(theme_dir))
+        .pipe(core.emit_pages(page_dir));
     return es.merge(s0, s1, s2);	
 }
 
-function watch_all() {
+function watch_all(theme_dir, page_dir) {
     var s0 = src();
-    var s1 = core.watch_pages()
-        .pipe(core.emit_dependent())
-    var s2 = core.watch_themes_all()
-        .pipe(core.gulp_theme())
-        .pipe(core.emit_pages());
+    var s1 = core.watch_pages(page_dir)
+        .pipe(core.emit_dependent(page_dir))
+    var s2 = core.watch_themes_all(theme_dir)
+        .pipe(core.gulp_theme(theme_dir))
+        .pipe(core.emit_pages(page_dir));
     return es.merge(s0, s1, s2);
 }
 
-function install() {
-    var s1 = gulp.src('./wikismith_themes/*/bower.json')
+function install(theme_dir) {
+    theme_dir = typeof theme_dir !== 'undefined' ? theme_dir : 'themes';
+    var s1 = gulp.src('./'+theme_dir+'/*/bower.json')
         .pipe(core.bower_install());
 
-    var s2 = gulp.src('./wikismith_themes/*/package.json')
+    var s2 = gulp.src('./'+theme_dir+'/*/package.json')
         .pipe(core.npm_install());
 
     return es.merge(s1, s2);
